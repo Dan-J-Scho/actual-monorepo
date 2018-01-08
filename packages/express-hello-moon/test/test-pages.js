7903f29bfd01dfb432019183ca8a84767f2429e6
bradmorton1 <33905003+bradmorton1@users.noreply.github.com>
2018-01-08 16:54:42 +0000

    added a test to determine status of localhost

--- packages/express-hello-moon/test/test-pages.js
+++ packages/express-hello-moon/test/test-pages.js
@@ -1,9 +1,15 @@
 var expect = require('chai').expect;
 var request = require('request');
 
-it('Main page content', function(done) {
+it('Should be able to read main page content', function(done) {
     request('http://localhost:3000', function(error, response, body) {
         expect(body).to.equal('Hello World!');
         done();
+    })
+})
+
+it('Should be able to find the page which the content is hosted on', function(done) {
+    request('http://localhost:3000', function(error, response, body) {
+        response.should.have.status(200);
     })
 })
